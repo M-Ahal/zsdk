19222dd9f25f5ec874854c770760160fc5e36f6e
M-Ahal <m.ahal@didata.org>
2025-11-12 18:31:57 +0100

    Typesafety for JSON de/serialization

--- lib/src/printer_response.dart
+++ lib/src/printer_response.dart
@@ -3,6 +3,7 @@ import 'enumerators/error_code.dart';
 import 'enumerators/status.dart';
 import 'printer_settings.dart';
 import 'status_info.dart';
+import 'typedefs.dart';
 
 /// Created by luis901101 on 2020-01-07.
 final class PrinterResponse {
@@ -15,17 +16,17 @@ final class PrinterResponse {
     : errorCode = errorCode ?? ErrorCode.unknown,
       statusInfo = statusInfo ?? const StatusInfo(Status.unknown, Cause.unknown);
 
-  Map<String, dynamic> toMap() => <String, dynamic>{
+  Json toMap() => {
     'errorCode': errorCode.name,
     'statusInfo': statusInfo.toMap(),
     'settings': settings?.toMap(),
     'message': message,
   };
 
-  factory PrinterResponse.fromMap(Map<dynamic, dynamic> map) => PrinterResponse(
-    errorCode: ErrorCode.values.byName(map['errorCode']),
-    statusInfo: map['statusInfo'] == null ? null : StatusInfo.fromMap(map['statusInfo']),
-    settings: map['settings'] == null ? null : PrinterSettings.fromMap(map['settings']),
-    message: map['message'],
+  factory PrinterResponse.fromMap(Json map) => PrinterResponse(
+    errorCode: ErrorCode.values.byName(map['errorCode'] as String),
+    statusInfo: map['statusInfo'] == null ? null : StatusInfo.fromMap(map['statusInfo'] as Json),
+    settings: map['settings'] == null ? null : PrinterSettings.fromMap(map['settings'] as Json),
+    message: map['message'] as String?,
   );
 }
